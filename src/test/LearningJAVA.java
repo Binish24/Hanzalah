39f52098dce3ec44d7e2903829e83828f6bba70f
nabiullahbenish <nabiullah_benash@yahoo.com>
2024-08-12 13:19:16 -0700

    my initial push of my project to github

---
+++ src/test/LearningJAVA.java
@@ -0,0 +1,23 @@
+package test;
+
+public class LearningJAVA {
+
+
+    public static void main (String [] args) {
+
+       System.out.println ('THIS IS MY WORLD')
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
